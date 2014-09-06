d04c891707c39672ad07c26d710d3919c05c58ff
Ajay Sharma <aj@ajsharma.com>
2014-09-06 01:45:12 +0000

    Working github login

---
+++ app/models/github_event.rb
@@ -0,0 +1,2 @@
+class GithubEvent < ActiveRecord::Base
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+
+  resources :github_events, :only => [ :index, :show ]
+
   get '/auth/:provider/callback' => 'sessions#create'
+  get '/auth/failure' => 'sessions#failure'
+
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
-  get '/auth/failure' => 'sessions#failure'
+
+  root to: 'github_events#index'
 end
