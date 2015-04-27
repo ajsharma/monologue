04520e46b1734d0d70b672fc78786c3a4a939b1d
Ajay Sharma <aj@ajsharma.com>
2015-04-27 16:01:50 +0000

    Show all users' issues by default on /issues page

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -4,21 +4,49 @@ class IssuesController < ApplicationController
   # GET /issues
   # GET /issues.json
   def index
-    @members = Github::Repo.repository_assignees current_user_github_access_token,
-      current_repository_full_name
+    @members = repository_members
+    @open_issues = open_issues
+    @closed_issues = closed_issues
+  end
 
-    @open_issues = Github::Issue.organization_issues current_user_github_access_token,
+  private
+
+  def current_github_username
+    params[:username] || current_user_github_username
+  end
+
+  def open_issues
+    scoped_members_names.flat_map do |member_name|
+      Github::Issue.organization_issues current_user_github_access_token,
       current_repository_full_name,
-      assignee: current_github_username,
+      assignee: member_name,
       sort: "updated",
       state: "open"
+    end
+  end
 
-    @closed_issues = Github::Issue.organization_issues current_user_github_access_token,
-      current_repository_full_name,
-      assignee: current_github_username,
-      sort: "updated",
-      since: 1.days.ago.iso8601.to_s,
-      state: "closed"
+  def closed_issues
+    scoped_members_names.flat_map do |member_name|
+      Github::Issue.organization_issues current_user_github_access_token,
+        current_repository_full_name,
+        assignee: member_name,
+        sort: "updated",
+        since: 1.days.ago.iso8601.to_s,
+        state: "closed"
+    end
+  end
+
+  def repository_members
+    @repository_members ||= Github::Repo.repository_assignees current_user_github_access_token,
+      current_repository_full_name
+  end
+
+  def scoped_members_names
+    if params[:username]
+      [current_github_username]
+    else
+      repository_members.map &:login
+    end
   end
 
 end
